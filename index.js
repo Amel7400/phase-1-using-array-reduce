b0c032ff085c315488930dccbeef349a88bb3d0e
AMEL BATOUCHE <amelbat7400@gmail.com>
2023-09-01 21:29:54 +0200

    reduce Assignement Completed

--- index.js
+++ index.js
@@ -1,3 +1,9 @@
 const batteryBatches = [4, 5, 3, 4, 4, 6, 5];
 
-// Code your solution here
+
+// Calculate totalBatteries using reduce()
+const totalBatteries = batteryBatches.reduce((total, batch) => total + batch, 0);
+
+// Export the totalBatteries variable so it's accessible in other parts of your code
+console.log(totalBatteries);
+
